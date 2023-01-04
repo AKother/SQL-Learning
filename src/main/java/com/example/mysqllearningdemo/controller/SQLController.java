3180cf23fb1dec8d6d10867db44a2c1146002fe9
Kother <1482089698@qq.com>
2023-01-04 18:22:43 +0800

    history

--- src/main/java/com/example/mysqllearningdemo/controller/SQLController.java
+++ src/main/java/com/example/mysqllearningdemo/controller/SQLController.java
@@ -1,12 +1,17 @@
 package com.example.mysqllearningdemo.controller;
 
 
+import com.alibaba.fastjson.JSON;
 import com.example.mysqllearningdemo.common.Result;
+import com.example.mysqllearningdemo.pojo.History;
 import com.example.mysqllearningdemo.pojo.vo.ExecuteSQLDTO;
+import com.example.mysqllearningdemo.service.impl.HistoryServiceImpl;
 import com.example.mysqllearningdemo.service.impl.SQLServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @Api(value = "SQL",tags = {"SQL"})
@@ -17,10 +22,20 @@ public class SQLController {
     @Autowired
     SQLServiceImpl sqlService;
 
+    @Autowired
+    HistoryServiceImpl historyService;
+
     @ApiOperation("执行SQL并拿到返回结果")
     @PostMapping("/execute")
     public Result executeSQL(@RequestBody ExecuteSQLDTO executeSQLDTO){
-        return sqlService.executeSQL(executeSQLDTO.getUserId(), executeSQLDTO.getSql());
+        Result result = sqlService.executeSQL(executeSQLDTO.getUserId(), executeSQLDTO.getSql());
+        History history = new History();
+        history.setSql(executeSQLDTO.getSql());
+        history.setResult(JSON.toJSONString(result.getData()));
+        history.setUserId(executeSQLDTO.getUserId());
+        history.setIsStar(0);
+        historyService.save(history);
+        return result;
     }
 
     @ApiOperation(value = "查看用户创建的表",notes = "返回的只是表名，此处表名即是表ID")
--- src/main/java/com/example/mysqllearningdemo/utils/MD5Utils.java
+++ src/main/java/com/example/mysqllearningdemo/utils/MD5Utils.java
@@ -14,4 +14,5 @@ public class MD5Utils {
         String newPassword = String.valueOf(new SimpleHash(hashAlgorithmName, password,salt,hashIterations));
         return newPassword;
     }
+
 }
